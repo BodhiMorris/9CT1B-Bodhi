6d8c1e7325b07e156a1c4274a5bad54bacf09e71
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-06-15 18:10:33 +1000

    Module 11 coding challenge

--- python-for-beginners/13 - Functions/code_challenge.py
+++ python-for-beginners/13 - Functions/code_challenge.py
@@ -5,7 +5,20 @@
 # operation: the word 'add' or 'subtract'
 # the function should return the result of the two numbers added or subtracted
 # based on the value passed in for the operator
-#
+
+def calculate(num1, operation, num2):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    else:
+        return('Nuh uh, put real opperation')
+
+num1 = int(input('First number '))
+opp = input('Operation please (add or subtract) ')
+num2 = int(input('Second number '))
+
+print(calculate(num1, opp, num2))
 # Test your function with the values 6,4, add 
 # Should return 10
 #
