f79f61b12f6bcde35e3cd18014ded5eb038c0db6
BodhiMorris <bodhi.morris@education.nsw.gov.au>
2024-06-16 21:07:53 +1000

    module 1.5 work

--- python-for-beginners/14 - Function parameters/code_challenge.py
+++ python-for-beginners/14 - Function parameters/code_challenge.py
@@ -6,11 +6,25 @@
 # the function should return the result of the two numbers added or subtracted
 # based on the value passed in for the operator
 #
+def calculate(num1, num2, opp = 'add'):
+    if opp == 'add':
+        return num1 + num2
+    elif opp == 'subtract':
+        return num1 - num2
+    else:
+        return 'I hate you'
+
+
+
 # Test your function using named notation passing in only the numbers 6 and 4
 # Should return 10
-#
+
+print(calculate(6, 4))
+
 # Test your function using named notation with the values 6,4, subtract 
 # Should return 2
-# 
+
+print(calculate(6, 4, 'subtract'))
+
 # BONUS: Test your function with the values 6, 4 and divide 
 # Have your function return an error message when invalid values are received
